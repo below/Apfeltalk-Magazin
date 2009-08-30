5f83fef25d02ed746c72976f4aa2499912efc5b1
Alexander von Below <Alex@vonBelow.Com>
2009-08-30 12:36:45 +0200

    Undeleted some important stuff ...

--- Classes/PodcastController.m
+++ Classes/PodcastController.m
@@ -19,5 +19,15 @@
 - (NSString *) documentPath {
 	return @"http://feeds2.feedburner.com/apfeltalk-small";
 }
+
+- (void)parser:(NSXMLParser *)parser didStartElement:(NSString *)elementName namespaceURI:(NSString *)namespaceURI qualifiedName:(NSString *)qName attributes:(NSDictionary *)attributeDict{      
+	if ([elementName isEqualToString:@"enclosure"]) {
+		currentElement = [elementName copy];
+		NSString *link = [attributeDict valueForKey:@"url"];
+		[item setLink:link];
+	}
+	else
+		[super parser:parser didStartElement:elementName namespaceURI:namespaceURI qualifiedName:qName attributes:attributeDict];
+}
 @end
 
