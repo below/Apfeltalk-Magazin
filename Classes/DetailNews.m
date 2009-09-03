f1c8b01b063a8f05d15810ea24cfdf602ea526d5
Andreas Rami <andreas.rami@gmail.com>
2009-09-03 23:11:01 +0200

    fixed bug where mail could only be sent when story is saved

--- Classes/DetailNews.m
+++ Classes/DetailNews.m
@@ -92,13 +92,15 @@
 	
     if ([self showSave] && [newsController isSavedStory:[self story]])
         [self setShowSave:NO];
-	
-    if (![self showSave]) {
-		if (buttonIdx == 0) {
-			[[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"mailto:me@me.com?subject=subject&body=TEXT"]];
-		}
-	}
 
+	
+	if (buttonIdx == 0) {
+		NSString *url = [NSString stringWithString: @"mailto:foo@example.com?cc=bar@example.com&subject=Greetings%20from%20Cupertino!&body=Wish%20you%20were%20here!"];
+		[[UIApplication sharedApplication] openURL: [NSURL URLWithString: url]];
+		
+		//[[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"mailto:me@me.com?subject=subject&body=TEXT"]];
+	}
+	
     [actionSheet release];
 }
 
